6d454e459cddfb5edc882606e5b64b3955d9f286
andela-lovelyntijesunimi-israel <lovelyn.tijesunimi-israel@andela.com>
2016-09-18 20:44:10 +0100

    Fix failing test cases

--- server/controllers/document.controller.js
+++ server/controllers/document.controller.js
@@ -3,7 +3,8 @@
 
   var User = require('./../models/user.js'),
   Role = require('./../models/role.js'),
-  Document = require('./../models/document.js');
+  Document = require('./../models/document.js'),
+  utils = require('./../controllers/utilities');
 
   /**
    * [funtion to return error message]
@@ -35,306 +36,288 @@
     });
   }
 
-/**
- * [function to create a document]
- * @param  {[http request object]} req [used to get the request query]
- * @param  {[http response object]} res [used to respond back to client]
- * @return {[json]}     [success message that document has been created]
- */
-  exports.createDocument = function(req, res) {
-    // checks if role exists
-    Role.findOne({
-      _id: req.body.role
-    }, function(err, role) {
-        if(err) {
-          res.send(err);
-          // if role is not found
-        } else if(!role) {
-            sendError(res, 404, 'Role not found. Create first', false);
-          } else {
-            //checks if user exists
-            getUser(req, res);
-            }
-      });
-  };
-/**
- * [function to check if user exists]
- * @param  {[http request object]} req [used to get request query]
- * @param  {[http response object]} res [used to respond back to client]
- * @return {[void]}     [...]
- */
-  function getUser(req, res) {
-    User.findOne({
-      _id: req.body.userId
-    }, function(err, user) {
-        if(err) {
-          res.send(err);
-          // if user is not found
-        } else if(!user) {
-          sendError(res, 404, 'User not found', false);
-        } else {
-          //check if document already exists
-          getDocument(req, res);
-          }
-       });
-  }
   /**
    * [function to check if user exists]
    * @param  {[http request object]} req [used to get request query]
    * @param  {[http response object]} res [used to respond back to client]
    * @return {[void]}     [...]
    */
-  function getDocument(req, res) {
-    Document.findOne({
-      title: req.body.title
-    }, function (err, doc) {
-        if (err) {
-          sendError(res, null, err, false);
-          // if document is found
-        } else if (doc) {
-          sendError(res, 409, 'Document already exists', false);
+    function getUser(req, res) {
+      User.findOne({
+        _id: req.body.userId
+      }, function(err, user) {
+          if(err) {
+            res.send(err);
+            // if user is not found
+          } else if(!user) {
+            sendError(res, 404, 'User not found', false);
           } else {
-              //create new document
-              createNewDoc(req).save(function (err, doc) {
-                if (err) {
-                  res.send(err);
-                } else {
-                  sendSuccess(res,
-                     'Document successfully created', doc);
-                }
-              });
+            //check if document already exists
+            getDocument(req, res);
             }
+         });
+    }
+    /**
+     * [function to check if user exists]
+     * @param  {[http request object]} req [used to get request query]
+     * @param  {[http response object]} res [used to respond back to client]
+     * @return {[void]}     [...]
+     */
+    function getDocument(req, res) {
+      Document.findOne({
+        title: req.body.title
+      }, function (err, doc) {
+          if (err) {
+            sendError(res, null, err, false);
+            // if document is found
+          } else if (doc) {
+            sendError(res, 409, 'Document already exists', false);
+            } else {
+                //create new document
+                createNewDoc(req).save(function (err, doc) {
+                  if (err) {
+                    res.send(err);
+                  } else {
+                    sendSuccess(res,
+                       'Document successfully created', doc);
+                  }
+                });
+              }
+         });
+    }
+  /**
+   * [function to create new document]
+   * @param  {[http request object]} req [used to get request query]
+   * @return {[json]}     [returns Document Object]
+   */
+    function createNewDoc(req) {
+       var newDoc = new Document({
+        title: req.body.title,
+        content: req.body.content,
+        userId: req.body.userId,
+        role: req.body.role
        });
-  }
-/**
- * [function to create new document]
- * @param  {[http request object]} req [used to get request query]
- * @return {[json]}     [returns Document Object]
- */
-  function createNewDoc(req) {
-     var newDoc = new Document({
-      title: req.body.title,
-      content: req.body.content,
-      userId: req.body.userId,
-      role: req.body.role
-     });
 
-     return newDoc;
-  }
+       return newDoc;
+    }
 
 
-  /**
-   * [function to return all available documents]
-   * @param  {[http request object]} req [used to get request query]
-   * @param  {[http response object]} res [used to respond back to client]
-   * @return {[json]}     [all documents available in the database]
-   */
-  exports.getAllDocuments = function (req, res) {
-    //find all available document(s)
-    Document.find({})
-    //parsing the limit
-    .limit(parseInt(req.params.limit))
-    .exec(function (err, docs) {
-      if (err) {
-        res.send(err);
-        // if no document(s) exists
-      } else if (!docs) {
-        sendError(res, 404, 'No documents available', false);
-      } else {
-          res.status(200).json(docs);
-      }
-    });
-  };
 
-  /**
-   * [function to get a document]
-   * @param  {[http request object]} req [used to get request query]
-   * @param  {[http response object]} res [used to respond back to client]
-   * @return {[json]}     [document with a specific Id]
-   */
-  exports.getADocument = function (req, res) {
-    // find a document with a specific id
-    Document.findById(req.params.id, function (err, doc) {
-      if (err) {
-        res.send(err);
-        //if no document is found
-      } else if (!doc) {
-        sendError(res, 404, 'Document not found', false);
+  module.exports = {
+    /**
+     * [function to create a document]
+     * @param  {[http request object]} req [used to get the request query]
+     * @param  {[http response object]} res [used to respond back to client]
+     * @return {[json]}     [success message that document has been created]
+     */
+      createDocument: function(req, res) {
+        // checks if role exists
+        Role.findOne({
+          _id: req.body.role
+        }, function(err, role) {
+            if(err) {
+              res.send(err);
+              // if role is not found
+            } else if(!role) {
+                sendError(res, 404, 'Role not found. Create first', false);
+              } else {
+                //checks if user exists
+                getUser(req, res);
+                }
+          });
+      },
 
-      } else {
-        res.status(200).json(doc);
-      }
-    });
-  };
+      /**
+       * [function to return all available documents]
+       * @param  {[http request object]} req [used to get request query]
+       * @param  {[http response object]} res [used to respond back to client]
+       * @return {[json]}     [all documents available in the database]
+       */
+      getAllDocuments: function (req, res) {
 
-  /**
-   * [function to get documents by its role]
-   * @param  {[http request object]} req [used to get request query]
-   * @param  {[http response object]} res [used to respond back to client]
-   * @return {[json]}     [documents created by a specific role]
-   */
-  exports.getDocumentByRole = function (req, res) {
-    //find documents with a specific role
-    Document.find({
-      role: req.params.role
-    })
-    //parsing the limit
-    .limit(parseInt(req.param.limit))
-    .exec(function (err, doc){
-      if (err) {
-        res.send(err);
-      //if no document is found
-      } else if (!doc) {
-        sendError(res, 404, 'Role has no document', false);
-      } else {
-        res.status(200).json(doc);
-      }
-    });
-  };
+        var searchTerm = req.query;
+        var query = utils.searchQuery(searchTerm);
 
-  /**
-   * [function to get documents by its role]
-   * @param  {[http request object]} req [used to get request query]
-   * @param  {[http response object]} res [used to respond back to client]
-   * @return {[json]}     [documents created by a specific role]
-   */
-  exports.getDocumentByTitle = function (req, res) {
-    //find documents with a specific role
-    Document.find({
-      title: req.params.title
-    })
-    //parsing the limit
-    .limit(parseInt(req.params.limit))
-    .exec(function (err, doc) {
-      if (err) {
-        res.send(err);
-        //if no document is found
-      } else if (doc.length < 1) {
-        sendError(res, 404, 'No documents found', false);
-      } else {
-        res.status(200).json(doc);
-      }
-    });
-  };
+        Document.find(query)
+        .limit(parseInt(searchTerm.limit) || parseInt(10))
+        .skip(parseInt(searchTerm.offset) || parseInt(0))
+        .exec(function (err, docs) {
+          if (err) {
+              res.send(err);
+              // if no document(s) exists
+            } else if (!docs) {
+              sendError(res, 404, 'No documents available', false);
+            } else {
+                res.status(200).json(docs);
+            }
+        });
 
-  /**
-   * [function to get documents of a specific user]
-   * @param  {[http request object]} req [used to get request query]
-   * @param  {[http response object]} res [used to respond back to client]
-   * @return {[json]}     [documents created by a specific user]
-   */
-  exports.getDocumentByUser = function (req, res) {
-    //find documents with a specific user
-    Document.find({
-      userId: req.params.userId
-    })
-    //parsing the limit
-    .limit(parseInt(req.params.limit))
-    .exec(function (err, doc) {
-      if (err) {
-        res.send(err);
-        //if no document is found
-      } else if (doc.length < 1) {
-        sendError(res, 404, 'User has no document', false);
-      } else {
-        res.status(200).json(doc);
-      }
-    });
-  };
+      },
 
-  /**
-   * [function to get documents of a specific user]
-   * @param  {[http request object]} req [used to get request query]
-   * @param  {[http response object]} res [used to respond back to client]
-   * @return {[json]}     [documents created by a specific user]
-   */
-
-  exports.getDocumentByDate = function(req, res) {
-    Document.find({
-      dateCreated: {
-            $gte: new Date(req.params.from),
-            $lt: new Date(req.params.to)
-          }
-    })
-    //parsing the limit
-    .limit(parseInt(req.params.limit))
-    .exec(function (err, doc) {
-      if (err) {
-        res.send(err);
-        //if no document is found
-      } else if (doc.length < 1) {
-        sendError(res, 404, 'No documents found', false);
-      } else {
-        res.status(200).json(doc);
-      }
-    });
-  };
-
-  exports.getDocumentByDescendingDate = function(req, res) {
-    Document.find({
-      dateCreated: {
-            $gte: new Date(req.params.from),
-            $lt: new Date(req.params.to)
-          }
-    })
-    //parsing the limit
-    .limit(parseInt(req.params.limit).sort({
-      dateCreated: 'descending'
-    }))
-    .exec(function (err, doc) {
-      if (err) {
-        res.send(err);
-        //if no document is found
-      } else if (doc.length < 1) {
-        sendError(res, 404, 'No documents found', false);
-      } else {
-        res.status(200).json(doc);
-      }
-    });
-
-
-  };
-
-  /**
-   * [function to edit a document]
-   * @param  {[http request object]} req [used to get request query]
-   * @param  {[http response object]} res [used to respond back to client]
-   * @return {[json]}     [success message that document has been edited]
-   */
-  exports.editDocument = function (req, res) {
-    //find a document with a specific id and update
-    Document.findByIdAndUpdate(
-      req.params.id, req.body,
-      function (err, doc) {
-        if(err){
-          sendError(res, null, 'Document update failed', false);
-          //if no document is found
-        } else if (!doc) {
+      /**
+       * [function to get a document]
+       * @param  {[http request object]} req [used to get request query]
+       * @param  {[http response object]} res [used to respond back to client]
+       * @return {[json]}     [document with a specific Id]
+       */
+      getADocument: function (req, res) {
+        // find a document with a specific id
+        Document.findById(req.params.id, function (err, doc) {
+          if (err) {
+            res.send(err);
+            //if no document is found
+          } else if (!doc) {
             sendError(res, 404, 'Document not found', false);
-        } else {
-            sendSuccess(res, 'Document successfully updated', true);
-        }
-      });
-  };
 
-  /**
-   * [function to deletr a document]
-   * @param  {[http request object]} req [used to get request query]
-   * @param  {[http response object]} res [used to respond back to client]
-   * @return {[json]}     [success message that document has been deleted ]
-   */
-  exports.deleteDocument = function (req, res) {
-    //find a document with a specific id and delete it
-    Document.findByIdAndRemove(req.params.id, function (err, doc) {
-      if (err) {
-        res.send(err);
-        //if no document is found
-      } else if (!doc) {
-        sendError(res, 404, 'Document not found', false);
-      } else {
-        sendSuccess(res, 'Document successfully deleted', true);
+          } else {
+            res.status(200).json(doc);
+          }
+        });
+      },
+
+      /**
+       * [function to get documents by its role]
+       * @param  {[http request object]} req [used to get request query]
+       * @param  {[http response object]} res [used to respond back to client]
+       * @return {[json]}     [documents created by a specific role]
+       */
+      getDocumentByRole: function (req, res) {
+        //find documents with a specific role
+        Document.find({
+          role: req.params.role
+        })
+        //parsing the limit
+        .limit(parseInt(req.param.limit))
+        .exec(function (err, doc){
+          if (err) {
+            res.send(err);
+          //if no document is found
+          } else if (!doc) {
+            sendError(res, 404, 'Role has no document', false);
+          } else {
+            res.status(200).json(doc);
+          }
+        });
+      },
+
+      /**
+       * [function to get documents of a specific user]
+       * @param  {[http request object]} req [used to get request query]
+       * @param  {[http response object]} res [used to respond back to client]
+       * @return {[json]}     [documents created by a specific user]
+       */
+      getDocumentByUser: function (req, res) {
+        //find documents with a specific user
+        Document.find({
+          userId: req.params.userId
+        })
+        //parsing the limit
+        .limit(parseInt(req.params.limit))
+        .exec(function (err, doc) {
+          if (err) {
+            res.send(err);
+            //if no document is found
+          } else if (doc.length < 1) {
+            sendError(res, 404, 'User has no document', false);
+          } else {
+            res.status(200).json(doc);
+          }
+        });
+      },
+
+      /**
+       * [function to get documents of a specific user]
+       * @param  {[http request object]} req [used to get request query]
+       * @param  {[http response object]} res [used to respond back to client]
+       * @return {[json]}     [documents created by a specific user]
+       */
+
+      getDocumentByDate: function(req, res) {
+        Document.find({
+          dateCreated: {
+              }
+        })
+        //parsing the limit
+        .limit(parseInt(req.params.limit))
+        .exec(function (err, doc) {
+          if (err) {
+            res.send(err);
+            //if no document is found
+          } else if (doc.length < 1) {
+            sendError(res, 404, 'No documents found', false);
+          } else {
+            res.status(200).json(doc);
+          }
+        });
+      },
+
+      getDocumentByDescendingDate: function(req, res) {
+        Document.find({
+          dateCreated: {
+                $gte: new Date(req.params.from),
+                $lt: new Date(req.params.to)
+              }
+        })
+        //parsing the limit
+        .limit(parseInt(req.params.limit).sort({
+          dateCreated: 'descending'
+        }))
+        .exec(function (err, doc) {
+          if (err) {
+            res.send(err);
+            //if no document is found
+          } else if (doc.length < 1) {
+            sendError(res, 404, 'No documents found', false);
+          } else {
+            res.status(200).json(doc);
+          }
+        });
+
+
+      },
+
+      /**
+       * [function to edit a document]
+       * @param  {[http request object]} req [used to get request query]
+       * @param  {[http response object]} res [used to respond back to client]
+       * @return {[json]}     [success message that document has been edited]
+       */
+      editDocument: function (req, res) {
+        //find a document with a specific id and update
+        Document.findByIdAndUpdate(
+          req.params.id, req.body,
+          function (err, doc) {
+            if(err){
+              sendError(res, null, 'Document update failed', false);
+              //if no document is found
+            } else if (!doc) {
+                sendError(res, 404, 'Document not found', false);
+            } else {
+                sendSuccess(res, 'Document successfully updated', true);
+            }
+          });
+      },
+
+      /**
+       * [function to deletr a document]
+       * @param  {[http request object]} req [used to get request query]
+       * @param  {[http response object]} res [used to respond back to client]
+       * @return {[json]}     [success message that document has been deleted ]
+       */
+      deleteDocument: function (req, res) {
+        //find a document with a specific id and delete it
+        Document.findByIdAndRemove(req.params.id, function (err, doc) {
+          if (err) {
+            res.send(err);
+            //if no document is found
+          } else if (!doc) {
+            sendError(res, 404, 'Document not found', false);
+          } else {
+            sendSuccess(res, 'Document successfully deleted', true);
+          }
+        });
       }
-    });
+
   };
 
  })();
--- server/routes/documents.routes.js
+++ server/routes/documents.routes.js
@@ -15,31 +15,13 @@
       router.route('/documents')
         .get(auth.authMiddleware, documentController.getAllDocuments);
 
-      //route to get all documents with a specified limit
-      router.route('/documents?limit:limit')
-      .get(auth.authMiddleware, documentController.getAllDocuments);
-
       //route to get all documents with a specific role
-      router.route('/documents/role/:role/:limit')
+      router.route('/documents/role/:role')
         .get(auth.authMiddleware, documentController.getDocumentByRole);
 
       //route to get all documents of a specific user
       router.route('/user/:userId/documents')
         .get(auth.authMiddleware, documentController.getDocumentByUser);
-
-      //route to get a document with a specific title
-      router.route('/documents?title:title')
-        .get(auth.authMiddleware, documentController.getDocumentByTitle);
-
-      //route to get documents at a particular date and a limit
-      router.route('/documents?date=createdAt/:limit')
-        .get(auth.authMiddleware,
-          documentController.getDocumentByDescendingDate);
-
-          //route to get documents at a particular date
-          router.route('/documents?createdAt:createdAt')
-            .get(auth.authMiddleware,
-              documentController.getDocumentByDate);
 
       //route to get a document by its Id
       router.route('/documents/:id')
